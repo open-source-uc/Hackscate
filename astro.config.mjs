fcbea212f37465d24287b930ab62023cfe849e5b
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-11-15 17:37:31 -0300

    feat: buttons on the header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,29 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es'],
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: false
-        },
-    },
-    redirects: {
-        '/': {
-            status: 301,
-            destination: '/en/'
-        }
-    },
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'es'],
+      routing: {
+          prefixDefaultLocale: true,
+          redirectToDefaultLocale: false
+      },
+  },
+
+  redirects: {
+      '/': {
+          status: 301,
+          destination: '/en/'
+      }
+  },
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "bg-destructive text-foreground hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
@@ -19,11 +19,13 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        hackscate: "bg-foreground text-background font-semibold hover:bg-foreground/90",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        hackscate: "rounded-tl-2xl rounded-br-2xl h-10 px-6 has-[>svg]:px-4",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
