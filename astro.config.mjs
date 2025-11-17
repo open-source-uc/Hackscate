f9fd7fd60cfc4d428abf06cf15713dceea4f282f
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-11-16 23:06:47 -0300

    feat: update OpenGraph meta tags, add og-image, and enhance Spanish translations for sponsors and schedule sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://hackscate.osuc.dev',
+    
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'es'],
